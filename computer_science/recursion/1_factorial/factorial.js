57c4505466b7b8577f159c31bcc9de7d5adf9632
Yashas <yashasb2008@gmail.com>
2026-09-16 13:59:10 +0530

    added base and recursive part to problem

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,6 +1,14 @@
-const factorial = function() {
-
+const factorial = function(n) {
+    if (n === 0) 
+        return 1;
+    else {
+        let result = n;
+        result *= factorial(n - 1);
+        return result;
+    }
 };
+
+console.log(factorial(3));
 
 // Do not edit below this line
 module.exports = factorial;
