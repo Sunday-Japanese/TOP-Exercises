268a1dad613a7794661e26599d2d2dbda57175f6
Yashas <yashasb2008@gmail.com>
2026-09-16 14:14:10 +0530

    in factorial added contarants , to detect undefined values

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,14 +1,22 @@
 const factorial = function(n) {
-    if (n === 0) 
+
+    if (n < 0 ||  !Number.isInteger(n)) {
+        return "undefined";
+    } else {
+         if (n === 0) 
         return 1;
     else {
         let result = n;
         result *= factorial(n - 1);
         return result;
     }
+}
+
+   
 };
 
-console.log(factorial(3));
+console.log(factorial("3"));
+
 
 // Do not edit below this line
 module.exports = factorial;
