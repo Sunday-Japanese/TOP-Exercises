bc7bb91763794a3fdfab2e6288121fd551d4ed93
Yashas <yashasb2008@gmail.com>
2026-09-17 21:57:49 +0530

    added my own unique solution to the 3rd problem

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,32 @@
-const totalIntegers = function() {
+const totalIntegers = function(object) {
+    if (typeof object !== 'object' && !Array.isArray(object)) return 'undefined NOT Arry nor Object' // guard clause
+
+  let objectValues = Object.values(object);
+  let intergers = 0;
+
+  const objCondition = typeof object === 'object' && object !== 'null';
+  const aryCondition = Array.isArray(object);
+
+  const objLengthCondition = (typeof object === 'object' && object !== 'null' ) ? objectValues.length : object.length; 
+
+  for (let i = 0; i < objLengthCondition ; i++) {                                                           // Here it is base part
+    if ( objCondition && Number.isInteger(objectValues[i]) ) {
+        intergers++;
+    } else if ( aryCondition &&  Number.isInteger(object[i])) {
+        intergers++;
+    } else if (objCondition) {                                                                               // Here is recursive part
+        intergers += totalIntegers(objectValues[i]);
+    } else if (aryCondition) {
+        intergers += totalIntegers(object[i]);
+    }
+  }
+
   
+  return intergers;
 };
   
+const test = [2,3,4,3,2,3,3,{a:3 , b : {c:4, e : [23,4324,23,3,23]}}];
+console.log(totalIntegers(test));
 // Do not edit below this line
 module.exports = totalIntegers;
+//  if (objCondition && typeof objectValues[i] === 'object' || Array.isArray(object))
