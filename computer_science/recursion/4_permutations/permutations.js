445ba5fc753bc8f5571a482c6709dca8920aa14d
Yashas <yashasb2008@gmail.com>
2026-09-18 16:22:03 +0530

    i am officially giving up on problem 4 , wasted an hour of my life on :<

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,6 @@
 const permutations = function(arry) {
     if (!Array.isArray(arry)) return "undefined";           // guard clause
-    let result = [];
+    let Arry = [];
 
     let prmuLength = 1;
     for (let i = 1; i <= arry.length; i++) {
@@ -8,28 +8,30 @@ const permutations = function(arry) {
     }
 
     let temp;
+    
     for (let i = 0; i < prmuLength; i++) {
-        
-        if (arry[i] !== arry[i - 1] && i > 0 ) {
+       
+        if (arry[i] !== arry[i - 1] && i > 0) {
             temp = arry[i - 1];
             arry[i - 1] = arry[i];
             arry[i] = temp;
         }
-       
+        console.log(arry);
+          Arry.push(arry);
     }
-     result.push(arry);
+    
 
     /*if (arry.length > 2) {
             result.push(permutations(arry));  
         }*/
 
-return result;
+return Arry;
   
 };
 
 const test = [2,3];
 
-console.log(permutations([3,2]));
+console.log(permutations(test));
   
 // Do not edit below this line
 module.exports = permutations;
